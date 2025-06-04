49ae56eb6bd561c43749f9692e026dc98918f78a
Ulia <lyanusia@gmail.com>
2025-06-04 14:17:16 +0200

    uploading and storing images

--- actions/posts.js
+++ actions/posts.js
@@ -1,6 +1,7 @@
 
 'use server';
 
+import { uploadImage } from "@/lib/cloudinary";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
 
@@ -27,11 +28,18 @@ export async function createPost(prevState, formData) {
         return { errors };
     }
 
+    let imageUrl;
+    try {
+        imageUrl = await uploadImage(image);
+    } catch (error) {
+        throw new Error('Image upload failed, post was not created. Please try again later.');
+    }
+
     await storePost({
-        imageUrl: '',
+        imageUrl,
         title,
         content,
-        userId: 1, // Assuming a static user ID for demonstration
+        userId: 1,
     });
 
     redirect('/feed');
