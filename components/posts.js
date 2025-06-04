678a58c45b130ea31a8ffed7e7fad4d05bb9dd8c
Ulia <lyanusia@gmail.com>
2025-06-04 15:04:24 +0200

    one of the way of using server actions

--- components/posts.js
+++ components/posts.js
@@ -1,3 +1,4 @@
+import { togglePostLikeStatus } from '@/actions/posts';
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 
@@ -19,7 +20,7 @@ function Post({ post }) {
             </p>
           </div>
           <div>
-            <LikeButton />
+            <LikeButton action={togglePostLikeStatus}/>
           </div>
         </header>
         <p>{post.content}</p>
